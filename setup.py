a949f4d196678319f01093ccc9ac796ea89cbc15
enamoria <phanhuykinh1@gmail.com>
2020-02-27 14:13:54 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@ setup(
     # license="BSD",
     url="https://github.com/enamoria/ZaG2P",
     install_requires=[
-          'python-Levenshtein @ git+https://github.com/ztane/python-Levenshtein', 'torch==1.1.0', 'torchtext==0.3.1', 'dill', 'visdom'
-      ],
+        # 'python-Levenshtein @ git+https://github.com/ztane/python-Levenshtein',
+        'torch==1.1.0', 'torchtext==0.3.1', 'dill', 'visdom', 'textdistance'
+    ],
     packages=['ZaG2P'],
     include_package_data=True
 )
